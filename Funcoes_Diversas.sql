f5f565daf6aee1729962b290a9b3cf41d4f99481
Ewerton Maik <ewertonmsa@hotmail.com>
2022-05-09 09:27:40 -0300

    Update Funcoes_Diversas.sql

--- Funcoes_Diversas.sql
+++ Funcoes_Diversas.sql
@@ -1,5 +1,4 @@
 -- O exemplo abaixo utiliza da Função ROW_NUMBER para gerar um sequência numérica automaticamente.
-
 SELECT 
   ROW_NUMBER() OVER(ORDER BY name ASC) AS Row#,
   name, recovery_model_desc
@@ -7,7 +6,6 @@ FROM sys.databases
 WHERE database_id < 5;
 
 -- Neste segundo exemplo, a sequência é quebrada / reiniciada a cada grudo definido pela coluna passada no parâmetro - PARTITION BY.
-
 SELECT 
   ROW_NUMBER() OVER(PARTITION BY recovery_model_desc ORDER BY name ASC) 
     AS Row#,
